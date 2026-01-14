a680cc0cd345874e93c6212ea677a2c3947a04ee
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:31:14 +0300

    Add files via upload

---
+++ ders_53/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+void calculate(char*);
+int main()
+{
+    char message[100];
+    puts("enter a sentence");
+    gets(message);
+    calculate(message);
+    return 0;
+}
+
+void calculate(char* ptr){
+    int letters[26],i,length;
+    char activeLetter;
+    length=strlen(ptr);
+
+    for(i=0;i<26;i++){
+        letters[i]=0;
+    }
+
+    for(i=0;i<length;i++){
+        activeLetter=tolower(*(ptr+i));
+        letters[activeLetter-97]++;
+    }
+
+    printf("Letter\tRepeat\n");
+    printf("-------\t------\n");
+    for(i=0;i<26;i++){
+        if(letters[i]!=0){
+            printf("%c\t%d\n",i+97,letters[i]);
+        }
+    }
+
+}
