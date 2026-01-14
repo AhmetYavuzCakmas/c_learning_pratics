a7f226d8113cd55bc6edd8465540af80c43c42c9
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:28:19 +0300

    Add files via upload

---
+++ ders_44/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+    char alphabet[70]="QWERTYUIOPÐSDFGHJKLZXCVBNM";
+
+    printf("length is %d\n",strlen(alphabet)); //26
+    printf("sizeof is %d\n",sizeof(alphabet)); //70
+
+    return 0;
+}
