de78890fad970fad3e25a8b53f2e8396a1f7f0ea
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:29:12 +0300

    Add files via upload

---
+++ ders_46/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    char message[100];
+
+    puts("please, enter a sentence:");
+    gets(message);
+
+    puts("\nyour sentence:");
+    puts(message);
+
+
+
+
+    return 0;
+}
