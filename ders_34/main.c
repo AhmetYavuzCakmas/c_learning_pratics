c12e6442a75f63b0b052ea4a91512f16d527bfbb
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:22:52 +0300

    Add files via upload

---
+++ ders_34/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.141592
+
+
+
+int main()
+{
+
+    int radius;
+    const float PI= 3.141592; // const = deðiþtirilemez
+
+
+
+
+    return 0;
+}
