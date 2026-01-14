a0ccfca830564b9d7f306ccfebbe52640f1f4b7f
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:16:53 +0300

    Add files via upload

---
+++ ders_18/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float exponentiation(float x,int y);
+
+int main()
+{
+    float base=0,result=0;//taban
+    int exponent;//üs
+    printf("enter base and exponent values: ");
+    scanf("%f%d",&base,&exponent);
+    result=exponentiation(base,exponent);
+    printf("Result: %f",result);
+    return 0;
+}
+
+float exponentiation(float x,int y){
+    float result=1;
+    int i;
+    if(y<0){
+        for(i=0;i<-y;i++){
+            result *= 1/x;
+        }
+    }else{
+        for(i =0;i<y;i++){
+            result *=x;
+        }
+    }
+    return result;
+
+}
