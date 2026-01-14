db6b82aa5c80e7db69a11f46c4e34c6d00ba9d1d
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:17:54 +0300

    Add files via upload

---
+++ ders_22/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char myKey;
+    printf("bir tusa basiniz: ");
+    myKey=getchar();
+    printf("%c tusuna bastiniz\n",myKey);
+    printf("Tusun ASCII kodu %d\n",myKey);
+
+
+
+    return 0;
+}
