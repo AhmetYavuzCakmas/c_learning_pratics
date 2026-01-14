769fb0b0c5382d231b5235740f68decccbe2a34b
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-01-14 17:14:53 +0300

    Add files via upload

---
+++ ders_12/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("enter a number: ");
+    scanf("%d",&num);
+
+    for(int i = 1;i<=num;i++){
+            for(int j = 1;j<=i;j++){
+                printf("%d",i);
+            }
+            printf("\n");
+        }
+
+    return 0;
+}
